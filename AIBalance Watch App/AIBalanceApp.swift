9e9b1834c0d7596aa0f302e9ac74d8cf212cdd89
zephyr <wenggaojian@live.cn>
2026-06-06 10:56:28 +0800

    Initial Commit

---
+++ AIBalance Watch App/AIBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIBalanceApp.swift
+//  AIBalance Watch App
+//
+//  Created by zephyr on 2026/6/6.
+//
+
+import SwiftUI
+
+@main
+struct AIBalance_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIBalance/AIBalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIBalanceApp.swift
+//  AIBalance
+//
+//  Created by zephyr on 2026/6/6.
+//
+
+import SwiftUI
+
+@main
+struct AIBalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
